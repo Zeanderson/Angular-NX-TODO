c0395cdbd2e29de33cb1ae840d931ca420d18e8c
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2025-02-18 17:05:30 -0600

    Main Angular setup TODO, styles.css figure out a way to have the app-main always just be inbetween the header and footer

---
+++ myngapp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  imports: [RouterModule, HeaderComponent, MainComponent, FooterComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'myngapp';
+}
